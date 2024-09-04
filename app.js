508bb991cfa9839d525ce7fdce19927674bd872f
Hardiing <nathanharding02@gmail.com>
2024-09-03 21:57:14 -0600

    Changed board size

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ let square
 
 
 function createBoard() {
-    for(let x = 0; x < 4; x++) {
-        for(let y = 0; y < 4; y++) {
+    for(let x = 0; x < 16; x++) {
+        for(let y = 0; y < 16; y++) {
             square = document.createElement("div")
             square.className = "tile"
             container.appendChild(square)
+            let number = document.createElement("p")
+            number.textContent = x
+            square.appendChild(number)
         }
     }
 }
