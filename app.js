7937eb5d9c6a949e1686d23388fd0818f7534e49
Hardiing <nathanharding02@gmail.com>
2024-09-09 21:47:24 -0600

    Struggling with variable grid count.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ gridBtn.addEventListener("click", () => {
     createBoard(input)
 })
 
+// create an initial 16x16 board
 function initialBoard() {
     for(let x = 0; x < 16; x++) {
         for(let y = 0; y < 16; y++) {
@@ -24,17 +25,46 @@ function initialBoard() {
     })
 }
 
+// remove existing board before creating board with input from user
+// function createBoard(numInput) {
+//     while (container.firstChild) {
+//         container.removeChild(container.lastChild)
+//     }
+//     for(let x = 0; x < numInput; x++) {
+//         for(let y = 0; y < numInput; y++) {
+//             square = document.createElement("div")
+//             square.className = "tile"
+//             container.appendChild(square)
+//         }
+//     }
+//     let squaresArray = document.querySelectorAll(".tile")
+
+//     squaresArray.forEach(function (square) {
+//         square.addEventListener("mouseover", function() {
+//         square.classList.add("hover")
+//         })
+//     })
+// }
+
 function createBoard(numInput) {
     while (container.firstChild) {
         container.removeChild(container.lastChild)
     }
-    for(let x = 0; x < numInput; x++) {
-        for(let y = 0; y < numInput; y++) {
-            square = document.createElement("div")
-            square.className = "tile"
-            container.appendChild(square)
-        }
+    for(let x = 0; x < numInput * numInput; x++) {
+        square = document.createElement("div")
+        square.className = "tile"
+        container.appendChild(square)
     }
+
+    let newHeight = newWidth = 360 / numInput ;
+    console.log(newHeight);
+    const newDivs = document.querySelectorAll('.tile');
+    newDivs.forEach(element => {
+        element.style.height = newHeight.toString() + "px";
+        element.style.width = newWidth.toString() + "px";
+      
+    })
+
     let squaresArray = document.querySelectorAll(".tile")
 
     squaresArray.forEach(function (square) {
